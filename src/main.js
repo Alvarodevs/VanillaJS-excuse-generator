46b1a19ce5d76385224033865e9871261a0c5044
Alvarodevs <alvaro.develops@gmail.com>
2020-11-30 16:20:36 +0000

    main js file commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#pressIt").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = generateExcuse();
+  });
 
-window.onload = function() {
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["my sister", "your dog", "my mother", "a guy"];
+  let action = ["stole", "broke", "hit", "kept", "sent"];
+  let object = ["my head", "the laptop", "my smartphone", "my house", "my car"];
+  let where = [
+    "at home",
+    "on the street",
+    "in the party",
+    "at the cafe",
+    "in Burger King"
+  ];
+  let when = ["yesterday", "last week", "on Saturday"];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let objIndx = Math.floor(Math.random() * object.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    object[objIndx] +
+    " " +
+    where[whereIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
